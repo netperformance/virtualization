a8a8fdc1a015b45b623a3f958293bab2c080c46f
aaron <mail@aaron>
2017-03-09 13:31:17 +0100

    changes

--- provision_db.sh
+++ provision_db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apt-get update
 
-# Password = vagrant
+# MySQL-Server inkl. debconf settings / PWD = vagrant
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
 sudo apt-get -y install mysql-server
+
