72a12ed078374d84a18fb06568fb09e4d3fce48b
aaron <mail@aaron>
2017-03-09 22:59:47 +0100

    changes

--- provision_app.sh
+++ provision_app.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Apache 2 + Anpassung der index.html
 apt-get install -y apache2
+service apache2 stop
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+service apache2 start
+cd /vagrant
+vagrant wget -q https://raw.githubusercontent.com/netperformance/virtualization/master/index.html
