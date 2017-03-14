b678addf024e34f2c6ce4e8fbd1571a2e599785e
aaron <mail@aaron>
2017-03-14 17:20:50 +0100

    changes

--- provision_lb.sh
+++ provision_lb.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Insert hostname
-#echo "192.168.10.3  app01" >> /etc/hosts
-#echo "192.168.10.4  app02" >> /etc/hosts
-#echo "192.168.10.5  db01" >> /etc/hosts
-
-# HAProxy
-apt-get install -y haproxy
+########################
+# see ansible Playbook #
+########################
