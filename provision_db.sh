848502934082194fa8183d4051d5994777c2e6fe
aaron <mail@aaron>
2017-03-14 17:20:45 +0100

    changes

--- provision_db.sh
+++ provision_db.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-apt-get update
+#apt-get update
 
-# MySQL-Server inkl. debconf settings / PWD = vagrant
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
-sudo apt-get -y install mysql-server
+# MySQL-Server Installation inkl. debconf settings / PWD = vagrant
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
+#sudo apt-get -y install mysql-server
 
+########################
+# see ansible Playbook #
+########################
