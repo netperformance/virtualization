5e6a42331c8619cd8774a0c7332c24f5e22e25bf
aaron <mail@aaron>
2017-03-14 17:20:22 +0100

    changes

--- provision_conf.sh
+++ provision_conf.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Ansible Installation 
+# Ansible Installation
 apt-get install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install ansible
 
+# git Installation
+apt-get install -y git
+
+# tree Installation
+apt-get install tree
