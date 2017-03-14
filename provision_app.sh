1294849aa821b90169110ebd0456d602003fa24c
netperformance <akreis@web.de>
2017-03-14 17:23:58 +0100

    Update provision_app.sh

--- provision_app.sh
+++ provision_app.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Apache 2 + Anpassung der index.html
-apt-get install -y apache2
-service apache2 stop
-rm -rf /var/www/html
-ln -s /vagrant /var/www/html
-service apache2 start
-cd /vagrant
-wget -q https://raw.githubusercontent.com/netperformance/virtualization/master/index.html
+#apt-get install -y apache2
+#service apache2 stop
+#rm -rf /var/www/html
+#ln -s /vagrant /var/www/html
+#service apache2 start
+#cd /vagrant
+#wget -q https://raw.githubusercontent.com/netperformance/virtualization/master/index.html
+
+########################
+# see ansible Playbook #
+########################
