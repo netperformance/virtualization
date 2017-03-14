f97352bdf5fec737de7f8f91a9441f0d35a7999e
aaron <mail@aaron>
2017-03-14 17:20:41 +0100

    changes

--- provision_dns.sh
+++ provision_dns.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Installation
-apt-get install -y bind9
-/etc/init.d/bind9 stop
+# DNS-Server Installation
+#apt-get install -y bind9
+#/etc/init.d/bind9 stop
 
+########################
+# see ansible Playbook #
+########################
